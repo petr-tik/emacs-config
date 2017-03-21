c6ba5f82724c680faa3ef1908200493ed2033878
petr-tik <petr-tik@users.noreply.github.com>
2017-03-21 23:11:33 +0000

    Add irony and my clang-format hook to Cpp mode

--- init.el
+++ init.el
@@ -219,3 +219,12 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+
+;;;;
+;; C
+;;;;
+(require 'irony)
+
+(add-hook 'c++-mode-hook 'irony-mode)
+(add-hook 'c++-mode-hook 'clang-format-enable-on-save)
